f3edf03c04184fcb2d82a57e19f79e7be66d4cba
João Luiz <Goenji48>
2022-10-17 22:18:20 -0300

    Arquivos

--- php/checkEmailAdmin.php
+++ php/checkEmailAdmin.php
@@ -1,4 +1,5 @@
 <?php
+include("encryptoURL.php");
     if(!isset($_SESSION)){
         session_start();
     }
@@ -6,7 +7,9 @@
     $_SESSION['email_administrador'] = $email1;
 
     if($_SERVER['REQUEST_URI'] == "/php/checkEmailAdmin.php?url=aHR0cHM6Ly90Y2Nhc3Npc3RlbmNpYS5oZXJva3VhcHAuY29tL3BocC9jaGVja0VtYWlsQWRtaW4ucGhw"){
-        header("Location: ../administrador.php");
+        $url = "../administrador.php";
+        $string = encode($url);
+        header("Location: $string");
     }
 
     printf($_SERVER['REQUEST_URI']);
